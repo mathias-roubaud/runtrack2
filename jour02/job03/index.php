0136d43a40a6bd84e5b297c197b4b0d6d6e63d8b
mathias <mathias.roubaud@laplateforme.io>
2022-11-17 14:52:06 +0100

    maj job03

---
+++ jour02/job03/index.php
@@ -0,0 +1,16 @@
+<?php
+for ($i=0;$i<=100;$i++){
+    
+    if ($i >=0 and $i<=20) {
+        echo "<i> $i </i> <br>";
+    }
+    elseif ($i >=25 and $i<=50) {
+        if ($i==42) {
+            echo "La Plateforme_<br>";
+        }
+        
+        else {echo "<u> $i </u> <br>";}
+    }
+    else{echo "$i <br>";}
+}
+?> 
