191ede187bbbf76568e3b7244c2a62ee4bee4c5b
mathias <mathias.roubaud@laplateforme.io>
2022-11-18 13:09:28 +0100

    maj job01

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -2,10 +2,10 @@
 $array = array (200, 204, 173, 98, 171, 404, 459);
 for ($i=0;$i<=6;$i++){
     if ($array[$i] % 2==0){
-        echo "$array[$i] est paire <br>";
+        echo "$array[$i] est paire<br>";
     }
     else {
-        echo "$array[$i] est impaire <br>";
+        echo "$array[$i] est impaire<br>";
     }
 }
 ?>
